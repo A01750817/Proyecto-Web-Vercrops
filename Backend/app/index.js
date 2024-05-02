4fa21b0c6b88b8d6792f94a1627c38beae39eb1d
Andrés Cabrera <119537317+a01798681@users.noreply.github.com>
2024-05-02 23:18:38 +0000

    cambios3

--- Backend/app/index.js
+++ Backend/app/index.js
@@ -1,14 +1,14 @@
 // Importa el framework Express.js
 import express from 'express'; 
-// Importa el módulo `path` para manipular rutas de archivos
+// Importa el módulo path para manipular rutas de archivos
 import path from 'path'; 
-// Importa la función `fileURLToPath` del módulo `url`
+// Importa la función fileURLToPath del módulo url
 import { fileURLToPath } from 'url'; 
 // Importa las funciones del controlador de autenticación
 import { login, register } from './controllers/authentication.controllers.js'; 
 // Importa las funciones del middleware de autorización
 import { soloAdmin, soloPublico } from './middlewares/authorization.js'; 
-// Importa el módulo `cookieParser` para analizar cookies
+// Importa el módulo cookieParser para analizar cookies
 import cookieParser from 'cookie-parser'; 
 
 // Define la ruta del directorio dos niveles arriba
@@ -25,6 +25,8 @@ console.log('Server on port', app.get('port'));
 
 // Configuración
 app.use(express.static(path.join(__dirname, 'Frontend', 'html_principal')));
+app.use(express.static(path.join(__dirname, 'Frontend', 'el_html_extras')));
+app.use(express.static(path.join(__dirname, 'los_scripts'))); // Servir archivos estáticos desde la carpeta 'los_scripts'
 // Analiza los cuerpos de las peticiones JSON entrantes
 app.use(express.json()); 
 // Analiza las cookies enviadas por el cliente
@@ -34,13 +36,6 @@ app.use(cookieParser());
 const rutaLoginHTML = path.join(__dirname, 'Frontend', 'html_principal', 'Login.html');
 app.get('/', soloPublico, (req, res) => res.sendFile(rutaLoginHTML));
 
-const rutaRegisterHTML = path.join(__dirname, 'Frontend', 'html_principal', 'SignUp.html');
-app.get('/register', soloPublico, (req, res) => res.sendFile(rutaRegisterHTML));
-
-const rutaAdminHTML = path.join(__dirname, 'Frontend', 'html_principal', 'indexP.html');
-console.log(rutaAdminHTML);
-app.get('/admin', soloAdmin, (req, res) => res.sendFile(rutaAdminHTML));
-
 app.get('/app', (req, res) => {
     // Ruta al archivo INDEX_charts.html
     const indexPath = path.join(__dirname, 'Frontend', 'el_html_extras', 'INDEX_charts.html');
@@ -49,7 +44,13 @@ app.get('/app', (req, res) => {
     res.sendFile(indexPath);
 });
 
-app.use(express.static(path.join(__dirname, 'Frontend', 'el_html_extras'))); // Servir archivos estáticos desde la carpeta 'el_html_extras'
+const rutaRegisterHTML = path.join(__dirname, 'Frontend', 'html_principal', 'SignUp.html');
+app.get('/register', soloPublico, (req, res) => res.sendFile(rutaRegisterHTML));
+
+const rutaAdminHTML = path.join(__dirname, 'Frontend', 'html_principal', 'indexP.html');
+console.log(rutaAdminHTML);
+app.get('/admin', soloAdmin, (req, res) => res.sendFile(rutaAdminHTML));
+
 // Rutas para API
 app.post('/api/login', login); // Ruta para la autenticación de usuario (login)
 app.post('/api/register', register); // Ruta para el registro de nuevo usuario
